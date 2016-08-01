d9ca78d89cf33919bc88f9b2c27d4c2123acbc4a
酿泉 <tioover@gmail.com>
2016-08-01 15:09:03 +0800

    这是盘好棋，我认输。
    以前的代码被黑暗吞噬，重新开始一局。

---
+++ LiHai/LiHai/common.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstddef>
+#include<cstdint>
+#include<type_traits>
+
+
+// Type redefinition
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using isize = std::make_signed_t<std::size_t>;
+using usize = std::size_t;
+using iptr = std::intptr_t;
+using uptr = std::uintptr_t;
+using f32 = float;
+using f64 = double;
--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-﻿# 厉害
+# 厉害
 
 看起来好像很厉害的数据结构和算法，用看起来很厉害的 C++ 来写。
 
-使用 Visual Studio 2015 Update 1 构筑。
+使用 Visual Studio 2015 Update 3 构筑。C++ 11 及以上特性。
 
 ## TODO
 
-* 堆
-* <del>AVL 树</del>
-* 图
-* 红黑树
-* 模板化和各种 C++ 黑科技
+[ ] 堆
+[x] AVL 树
+[ ] 图
+[ ] 红黑树
+[ ] 模板化和各种 C++ 黑科技
 
 ## 协议
 
-[WTFPL](https://en.wikipedia.org/wiki/WTFPL) 
-
-你他喵想干啥就干啥好了。
+[WTFPL](https://en.wikipedia.org/wiki/WTFPL)  喵的你想干啥就干啥好了。
